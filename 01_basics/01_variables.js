35da3b0c545bc2a9512f8cc57bc5e2a7966ebbbe
SahithiManisha <144056015+SahithiManisha@users.noreply.github.com>
2024-07-20 15:03:24 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "manisha01@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+
+accountEmail = "sahithi03@gmail.com"
+accountPassword = "2344"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity])
